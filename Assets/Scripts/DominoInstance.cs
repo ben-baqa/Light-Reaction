d2753ae4fb1364435509eb095c5c43000deea820
Benjamin Nielsen <56326234+ben-baqa@users.noreply.github.com>
2021-08-15 11:45:15 -0600

    Game manager (keeps track of dominos remaining/win state) player fixes and footstep noises

---
+++ Assets/Scripts/DominoInstance.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DominoInstance : MonoBehaviour
+{
+    public static bool allDown = false;
+
+    public float yDownThreshhold = 1f;
+    public bool down = false;
+
+    private float yStart;
+
+    private void Start()
+    {
+        yStart = transform.position.y;
+    }
+
+    void Update()
+    {
+        allDown = true;
+    }
+
+    private void LateUpdate()
+    {
+        if (down)
+            return;
+        down = transform.position.y - yStart > yDownThreshhold;
+        allDown &= down;
+    }
+}
